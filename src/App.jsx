a30348b6be54c0e1e2b2f4481989d8a0abfad8c2
Iata Anderson <96217918+IataAnderson@users.noreply.github.com>
2022-05-25 08:54:52 -0300

    criando componente conteudo

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Main from './Componentes/Main'
+import Conteudo from './Componentes/Conteudo'
 
 function App() {
     return (
     <div className="App">
       <Main />
       
-      <div className='conteudo'>
-        <div className='center'>
-          <div className='conteudo-single'>
-            <h3>Titulo da chamada</h3>
-            <p> Aqui o meu conteúdo de exemplo</p>
-          </div>
-          <div className='conteudo-single'>
-            <h3>Titulo da chamada</h3>
-            <p> Aqui o meu conteúdo de exemplo</p>
-          </div>
-          <div className='conteudo-single'>
-            <h3>Titulo da chamada</h3>
-            <p> Aqui o meu conteúdo de exemplo</p>
-          </div>
-          <div className='conteudo-single'>
-            <h3>Titulo da chamada</h3>
-            <p> Aqui o meu conteúdo de exemplo</p>
-          </div>
-        </div>
-
-      </div>
+      <Conteudo />
       
     </div>
   )
---
+++ src/Componentes/Conteudo.jsx
@@ -0,0 +1,27 @@
+function Conteudo(){
+    return(
+        <div className='conteudo'>
+        <div className='center'>
+          <div className='conteudo-single'>
+            <h3>Titulo da chamada</h3>
+            <p> Aqui o meu conteúdo de exemplo</p>
+          </div>
+          <div className='conteudo-single'>
+            <h3>Titulo da chamada</h3>
+            <p> Aqui o meu conteúdo de exemplo</p>
+          </div>
+          <div className='conteudo-single'>
+            <h3>Titulo da chamada</h3>
+            <p> Aqui o meu conteúdo de exemplo</p>
+          </div>
+          <div className='conteudo-single'>
+            <h3>Titulo da chamada</h3>
+            <p> Aqui o meu conteúdo de exemplo</p>
+          </div>
+        </div>
+
+      </div>
+    )
+}
+
+export default Conteudo
