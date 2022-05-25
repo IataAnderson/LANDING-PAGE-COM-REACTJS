812b267fbc06006c94f01190600341296a91d84c
Iata Anderson <96217918+IataAnderson@users.noreply.github.com>
2022-05-25 08:45:03 -0300

    criando componente Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Main from './Componentes/Main'
 
 function App() {
     return (
     <div className="App">
-      <div className='main'>
-        <div className='center'>
-        <div className='menu'>
-          <div className='logo'>
-            <h3>Resilia Educação</h3>
-          </div>
-          <div className='item-menu'>
-            <a href="#">Login</a>
-          </div>
-        </div>
-
-
-        <div className='form'>
-        <h2>Entre em contato!</h2>
-          <form>            
-            <div className='items-form'>
-              <input placeholder='Nome' type="text" />
-              <input placeholder='sobrenome' type="text" />
-              <input placeholder='Email' type="text" />
-              <input placeholder='CPF' type="text" />
-              <input type="submit" />
-            </div>
-          </form>
-
-        </div>
-
-        </div>
-
-      </div>
+      <Main />
+      
       <div className='conteudo'>
         <div className='center'>
           <div className='conteudo-single'>
